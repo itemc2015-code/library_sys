712f2521ab0cc7f6a3589813b5344ed94bcd9c41
mark <it.emc2015@gmail.com>
2025-09-29 16:20:25 +0800

    add tables

---
+++ lib_sys.py
@@ -0,0 +1,15 @@
+import mysql.connector
+
+mysqldb = mysql.connector.connect(
+    host = "localhost",
+    user = "root",
+    passwd = "123456",
+    database = "lib_sys"
+)
+mysql_cursor = mysqldb.cursor()
+
+c_db = ("show tables")
+mysql_cursor.execute(c_db)
+
+for l in mysql_cursor:
+    print(l)
